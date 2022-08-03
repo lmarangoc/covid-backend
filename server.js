5011a175028bcc74c38952db738115df2b333ba7
Laura Arango <lmarangoc@gmail.com>
2022-08-03 17:09:02 -0500

    feat: server.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 import Express from "express";
+import Cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db/db.js";
+import userRoutes from "./views/users/routes.js";
+
+dotenv.config({ path: "./.env" });
 
 const app = Express();
+app.use(Express.json());
+app.use(Cors());
 
-app.listen(5000, () => {
-  console.log("Listening port 5000");
-});
+app.use(userRoutes);
+
+const main = () => {
+  return app.listen(process.env.PORT, () => {
+    console.log(`Listening port ${process.env.PORT}`);
+  });
+};
+
+connectDB(main);
