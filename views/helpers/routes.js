00b4bd9fed93029244d84f45413660d97a216e21
Laura Arango <lmarangoc@gmail.com>
2022-08-18 00:33:33 -0500

    fix: routes

--- views/helpers/routes.js
+++ views/helpers/routes.js
@@ -17,12 +17,12 @@ const genericCallback = (res) => (err, result) => {
   }
 };
 
-caseRoutes.route("/helper/managecase").get((req, res) => {
+caseRoutes.route("/helper").get((req, res) => {
   console.log("Someone did get on the /case route");
   queryAllCases(genericCallback(res));
 });
 
-caseRoutes.route("/helper/managecase/:id").get((req, res) => {
+caseRoutes.route("/helper/:id").get((req, res) => {
   console.log("Someone did get on the /case route");
   consultCase(req.params.id, genericCallback(res));
 });
@@ -31,11 +31,11 @@ caseRoutes.route("/helper/registercase").post((req, res) => {
   createCase(req.body, genericCallback(res));
 });
 
-caseRoutes.route("/helper/managecase/:id").patch((req, res) => {
+caseRoutes.route("/helper/:id").patch((req, res) => {
   editCase(req.params.id, req.body, genericCallback(res));
 });
 
-caseRoutes.route("/helper/managecase/:id").delete((req, res) => {
+caseRoutes.route("/helper/:id").delete((req, res) => {
   deleteCase(req.params.id, genericCallback(res));
 });
 
